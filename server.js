8b35cc8679be9b2cdf2ea48998cb2d7a607cc683
aleydarachma-rgb <aleydarachma@gmail.com>
2026-05-06 11:16:33 +0700

    hems

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // --- 1. Database Connection (Disesuaikan untuk Docker) ---
 const db = mysql.createConnection({
-    host: '10.0.170.189', // <--- PENTING: Pakai IP Gateway Docker agar bisa akses MySQL di VPS
+    host: 'db', // <--- PENTING: Pakai IP Gateway Docker agar bisa akses MySQL di VPS
     user: 'root',
     password: process.env.DB_PASSWORD, 
     database: 'db_cloudku',
